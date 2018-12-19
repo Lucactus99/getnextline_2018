a407e392b873de8fa7747d1666e3d347b9541d9c
LUCAS DE CURTIS <lucas.de-curtis@epitech.eu>
2018-12-19 19:52:45 +0100

    added static var to do multiple calls to get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -11,12 +11,22 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+int my_strlen(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0') {
+        i++;
+    }
+    return (i);
+}
+
 char *my_realloc(char *ptr)
 {
     char *temp = ptr;
     int i = 0;
 
-    ptr = malloc(sizeof(ptr) * READ_SIZE);
+    ptr = malloc(sizeof(char) * (my_strlen(ptr) + READ_SIZE));
     while (temp[i]) {
         ptr[i] = temp[i];
         i++;
@@ -29,28 +39,28 @@ void my_putchar(char c)
     write(1, &c, 1);
 }
 
-void move_ptr(char **str, int count)
+char *read_line(char *str, int sizeStr)
 {
-    for (int i = 0; i < count; i++) {
-        **str++;
-    }
+
 }
 
-char *read_line(int fd)
+char *get_next_line(int fd)
 {
     char *str = malloc(sizeof(char) * READ_SIZE);
     char *buffer = malloc(sizeof(char) * READ_SIZE);
-    static int count = 0;
-    int size = read(fd, buffer, READ_SIZE);
+    static char over[READ_SIZE];
+    int count = 0;
+    int size;
     int sizeStr = 0;
 
-    buffer[size] = '\0';
+    for (int i = 0; over[i] != 0; i++) {
+        str[i] = over[i];
+        sizeStr++;
+    }
     while (*buffer != '\n') {
-        if (sizeStr > 0) {
-            size = read(fd, buffer, READ_SIZE);
-            str = my_realloc(str);
-            buffer[size] = '\0';
-        }
+        size = read(fd, buffer, READ_SIZE);
+        str = my_realloc(str);
+        buffer[size] = '\0';
         count = 0;
         for (; *buffer != '\n' && count < size; *buffer++) {
             str[sizeStr] = *buffer;
@@ -58,14 +68,10 @@ char *read_line(int fd)
             sizeStr++;
         }
     }
+    *buffer++;
+    for (int i = 0; *buffer != '\0'; i++) {
+        over[i] = *buffer;
+        *buffer++;
+    }
     return (str);
-}
-
-char *get_next_line(int fd)
-{
-    char *s = read_line(fd);
-
-    if (fd != -1)
-        close(fd);
-    return (s);
 }
